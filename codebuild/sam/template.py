5e63bf252c1defbe4ed74e10f2c002b9a5a3ebc3
Josh <jowillia@gmail.com>
2018-08-24 11:21:03 -0700

    creating a role

--- codebuild/sam/template.py
+++ codebuild/sam/template.py
@@ -1,13 +1,61 @@
-from troposphere import Template, Parameter
+from troposphere import Template, Parameter, Sub
 from troposphere.sqs import Queue
 
+from awacs.aws import Action, Allow, PolicyDocument, Principal, Statement, Policy
+from troposphere.iam import Role, Policy as iamPolicy
 from troposphere.s3 import Bucket
+
+from awacs import sts , s3, iam, codebuild, logs
 
 import json
 
+STACK_NAME = Sub('${AWS::StackName}')
+
+def code_build_service_role(artifact_bucket):
+    return Role('CodeDeployRole', 
+        RoleName=Sub('${AWS::StackName}-CodeDeploy-${AWS::Region}'),
+        AssumeRolePolicyDocument=assume_role_policy_document("codebuild.amazonaws.com"),
+        Policies=[iamPolicy(
+            PolicyName=STACK_NAME,
+            PolicyDocument=Policy(
+                Statement=[
+                    Statement(
+                        Effect=Allow,
+                        Resource= [ '*' ],
+                        Action=[
+                            logs.CreateLogGroup,
+                            logs.CreateLogStream,
+                            logs.PutLogEvents,
+                        ],
+                    ),
+                    Statement(
+                        Effect=Allow,
+                        Resource= [ Sub("arn:aws:s3:::${ArtifactBucket}/*") ],
+                        Action=[
+                            s3.GetObject,
+                            s3.GetObjectVersion,
+                            s3.PutObject,
+                        ],
+                    ),
+                ] 
+            )
+        )]
+    )
+
+def assume_role_policy_document(service_prinicpal):
+    return Policy(
+        Statement=[
+            Statement(
+                Effect=Allow,
+                Action=[sts.AssumeRole],
+                Principal=Principal("Service", [service_prinicpal]),
+            )
+        ]
+    )
+
 def create_template():
     t = Template()
-
+    
 
     oauth_token = t.add_parameter(Parameter(
         "GitHubOAuthToken",
@@ -33,8 +81,9 @@ def create_template():
     ))
 
 
-    artifact_bucket =  t.add_resource(Bucket('ArtifactBucket',
-    ))
+    artifact_bucket =  t.add_resource(Bucket('ArtifactBucket'))
+    
+    cd_role = t.add_resource(code_build_service_role(artifact_bucket))
 
     return t.to_dict()
 
